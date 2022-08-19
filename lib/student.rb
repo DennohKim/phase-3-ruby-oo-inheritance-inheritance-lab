4216efdcbfc0782ef4d9769649c7a61d7821411a
Dennis Kimathi <kimathidennis38@gmail.com>
2022-08-19 12:26:07 +0300

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,25 @@
-class Student 
+class User 
+    attr_accessor :first_name, :last_name
+
+    KNOWLEDGE = []
+
+    def initialize
+        @knowledge = []
+
+    end
+
+    
+
+end
+class Student < User
+    def learn(knowledge)
+        @knowledge << "Ruby framework Rails gem bundle update"
+    end
+
+    def knowledge
+        "Javascript Ember Elixir knowledge"
+        
+    end
+
 
 end
